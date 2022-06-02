235ecf7f82170bd731a2781051c6c19fe011250b
clemenshog <20182010@atn.ac.at>
2022-06-02 14:09:16 +0200

    first commit

---
+++ Data/Images.cs
@@ -0,0 +1,31 @@
+﻿namespace BlazorServerApp.Data
+{
+    public class Images
+    {
+        public List<string> GetImages()
+        {
+            List<string> images = new List<string>();
+            for (int i = 0; i < 10; i++)
+            {
+
+                string path = Directory.GetCurrentDirectory() + "\\wwwroot\\images\\" + i + ".jpg";
+                string path2 = "/images/" + i + ".jpg";
+                if (File.Exists(path))
+                {
+                    images.Add(path2);
+                }
+            }
+            return images;
+        }
+
+        public int CountFiles()
+        {
+
+            DirectoryInfo path = new DirectoryInfo(Directory.GetCurrentDirectory() + "\\wwwroot\\images\\");
+
+            int fileLength = path.GetFiles().Length;
+
+            return fileLength;
+        }
+    }
+}
---
+++ Data/Print.cs
@@ -0,0 +1,19 @@
+﻿namespace BlazorServerApp.Data
+{
+    public class Print
+    {
+
+        public void PrintImage(int amount, string file)
+        {
+            System.Diagnostics.Process process = new System.Diagnostics.Process();
+            System.Diagnostics.ProcessStartInfo startInfo = new System.Diagnostics.ProcessStartInfo();
+            startInfo.WindowStyle = System.Diagnostics.ProcessWindowStyle.Hidden;
+            //startInfo.UseShellExecute = true;
+            startInfo.FileName = "/bin/bash";
+            startInfo.Arguments = "/home/pi/print.sh lp -o media=Custom.4x6in -n " + amount + " " + file;
+            process.StartInfo = startInfo;
+            process.Start();
+        }
+
+    }
+}
